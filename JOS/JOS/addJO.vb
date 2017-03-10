01f44030d127723030f771c16e97302927af5c2c
jcunanan05 <j_cunanan05@yahoo.com>
2017-03-10 13:52:38 +0800

    edited addJO
    added void method goodbyeMessage()

--- JOS/JOS/addJO.vb
+++ JOS/JOS/addJO.vb
@@ -3,4 +3,9 @@
     Private Sub addJO_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
+
+    'void method'
+    Private Sub goodbyeMessage()
+    	MsgBox("Good bye!")
+    End Sub
 End Class
